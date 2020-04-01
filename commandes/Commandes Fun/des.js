8b640b8ab6e9a0b2e9d0ae0c24a2ea1b53b2a53c
SonMaxime <47661880+SonMaxime@users.noreply.github.com>
2020-04-01 13:12:46 +0200

    Update des.js

--- commandes/Commandes Fun/des.js
+++ commandes/Commandes Fun/des.js
@@ -3,7 +3,6 @@ module.exports = {
     aliases: ["des"],
     category: "Commandes Fun",
     description: 'Tourne un dès avec 6 faces par défaut.',
-    usage: '[nombre de faces]',
     run: async (client, message, args) => {
         if(!args[0]) {
             args[0] = 6;
