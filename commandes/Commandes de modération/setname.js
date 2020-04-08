aa226ccc9b7c1a3c7d7d562380d1fcacaf2682c0
SonMaxime <47661880+SonMaxime@users.noreply.github.com>
2020-04-08 10:13:48 +0200

    Update setname.js

--- commandes/Commandes de modération/setname.js
+++ commandes/Commandes de modération/setname.js
@@ -1,12 +1,18 @@
-    if(args.length < 2){
-        message.channel.send("no new name specified");
-        return;
+module.exports = {
+    name: "setname",
+    category: "Commandes de modération",
+    description: "Change le nom du bot.",
+    run: async(mesage, client, args) => {
+        if(args.length < 2){
+            message.channel.send("y'a pas de nom.");
+            return;
+        }
+    
+        var username = "";
+        for(var i = 1; i < args.length; i++){
+            username += args[i] + " ";
+        }
+        client.user.setUsername(username)
+            .then(user => console.log("--> " + user.username));
     }
-
-    var username = "";
-    for(var i = 1; i < args.length; i++){
-        username += args[i] + " ";
-    }
-    client.user.setUsername(username)
-        .then(user => console.log("--> New username set: " + user.username));
-};
+}
