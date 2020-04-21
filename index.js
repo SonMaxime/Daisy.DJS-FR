0b9b9173f578efe864f5f06b38d08b5c7a3b20d2
SonMaxime <47661880+SonMaxime@users.noreply.github.com>
2020-04-21 14:36:46 +0200

    Update index.js

--- index.js
+++ index.js
@@ -25,7 +25,6 @@ config({
 
 client.on("ready", () => {
     console.log(`Hey, ${client.user.username} est en ligne`);
-
     client.user.setPresence({
         status: "online",
         game: {
@@ -33,59 +32,6 @@ client.on("ready", () => {
             type: "STREAMING"
         }
     }); 
-});
-
-const usersMap = new Map();
-const LIMIT = 5;
-const TIME = 7000;
-const DIFF = 3000;
-
-client.on('message', message => {
-  if(message.author.bot) return;
-  if(usersMap.has(message.author.id)) {
-    const userData = usersMap.get(message.author.id);
-    const { lastMessage, timer } = userData;
-    const difference = message.createdTimestamp - lastMessage.createdTimestamp;
-    let msgCount = userData.msgCount;
-    console.log(difference);
-    if(difference > DIFF) {
-      clearTimeout(timer);
-      console.log('Cleared timeout');
-      userData.msgCount = 1;
-      userData.lastMessage = message;
-      userData.timer = setTimeout(() => {
-        usersMap.delete(message.author.id);
-        console.log('Removed from RESET.');
-      }, TIME);
-      usersMap.set(message.author.id, userData);
-    }
-    else {
-      ++msgCount;
-      if(parseInt(msgCount) === LIMIT) {
-        const role = message.guild.roles.cache.get('');
-        message.member.roles.add(role);
-        message.channel.send('Vous avez été mute.');
-        setTimeout(() => {
-          message.member.roles.remove(role);
-          message.channel.send('Vous avez été demute.');
-        }, TIME);
-      } else {
-        userData.msgCount = msgCount;
-        usersMap.set(message.author.id, userData);
-      }
-    }
-  }
-  else {
-    let fn = setTimeout(() => {
-      usersMap.delete(message.author.id);
-      console.log('Removed from map.');
-    }, TIME);
-    usersMap.set(message.author.id, {
-      msgCount: 1,
-      lastMessage: message,
-      timer: fn
-    });
-  }
 });
 
    // Système d'ajout de rôle et de bienvenue automatique
