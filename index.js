b9a3804b4b7c735b40f194157214753ec7a0f857
SonMaxime <47661880+SonMaxime@users.noreply.github.com>
2020-05-01 15:29:34 +0200

    Update index.js

--- index.js
+++ index.js
@@ -139,6 +139,34 @@ client.on("message", async message => {
     
     let command = client.commands.get(cmd);
     if (!command) command = client.commands.get(client.aliases.get(cmd));
+    
+    let xpAdd = Math.floor(Math.random() * 7) + 8;
+    console.log(xpAdd);
+
+  if(!xp[message.author.id]){
+    xp[message.author.id] = {
+      xp: 0,
+      level: 1
+    };
+  }
+
+
+  let curxp = xp[message.author.id].xp;
+  let curlvl = xp[message.author.id].level;
+  let nxtLvl = xp[message.author.id].level * 300;
+  xp[message.author.id].xp =  curxp + xpAdd;
+  if(nxtLvl <= xp[message.author.id].xp){
+    xp[message.author.id].level = curlvl + 1;
+    let lvlup = new Discord.RichEmbed()
+    .setTitle("Level Up!")
+    .setColor(purple)
+    .addField("New Level", curlvl + 1);
+
+    message.channel.send(lvlup).then(msg => {msg.delete(5000)});
+  }
+  fs.writeFile("./xp.json", JSON.stringify(xp), (err) => {
+    if(err) console.log(err)
+  });
 
     if (command) 
         command.run(client, message, args);
