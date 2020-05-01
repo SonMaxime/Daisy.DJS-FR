046bce34b7c5f4684c2ae420edfcbfe8bc39a313
SonMaxime <47661880+SonMaxime@users.noreply.github.com>
2020-05-01 15:51:23 +0200

    Delete skip.js

--- commandes/Commandes musicales/skip.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: "skip",
-    category: "Commandes musicales",
-    description: "Skip un son dans la queue ou le son joué actuellement.",
-    run: async (client, message, args) => {
-        message.channel.send('Bientôt.');
-    },
-};
