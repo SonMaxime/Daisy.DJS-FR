fa2d98f22a43df472c42abab828d71d7f47f1db0
SonMaxime <47661880+SonMaxime@users.noreply.github.com>
2020-05-01 15:50:55 +0200

    Delete pause.js

--- commandes/Commandes musicales/pause.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: "pause",
-    category: "Commandes musicales",
-    description: "Pause un son en cours dans une commande vocale.",
-    run: async (client, message, args) => {
-        message.channel.send('Bientôt.');
-    },
-};
