ede9bcfe6e7b6664df2e7fb8944eb835937053d7
SonMaxime <47661880+SonMaxime@users.noreply.github.com>
2020-05-01 15:51:33 +0200

    Delete queue.js

--- commandes/Commandes musicales/queue.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: "queue",
-    category: "Commandes musicales",
-    description: "Affiche la liste des sons dans la queue.",
-    run: async (client, message, args) => {
-        message.channel.send('Bientôt.');
-    },
-};
