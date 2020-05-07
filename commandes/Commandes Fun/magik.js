dcd4047a832dea237d566f4a2085e8e7d72b4237
SonMaxime <47661880+SonMaxime@users.noreply.github.com>
2020-05-07 12:41:08 +0200

    Update magik.js

--- commandes/Commandes Fun/magik.js
+++ commandes/Commandes Fun/magik.js
@@ -2,7 +2,7 @@ module.exports = {
     name: "magik",
     category: "Commandes Fun",
     description: "Modifie votre pp façon mAgIk",
-    run: async function (message, client, args) {
+    run: async (message, client, args) => {
     let avatarurl = message.mentions.length > 0 ? message.mentions[0].staticAvatarURL : message.author.staticAvatarURL
     if (['jpg', 'jpeg', 'gif', 'png', 'webp'].some(x => args.join(' ').includes(x))) {
       avatarurl = args.join(' ').replace(/gif|webp/g, 'png')
