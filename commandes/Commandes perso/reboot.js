6a8c22666c4739271e66c32b7a4e90c927c5e4bf
SonMaxime <47661880+SonMaxime@users.noreply.github.com>
2020-05-11 14:18:25 +0200

    Update reboot.js

--- commandes/Commandes perso/reboot.js
+++ commandes/Commandes perso/reboot.js
@@ -5,7 +5,7 @@ module.exports = {
     run: async(client, message, args) => {
         if (message.author.id !== "492402867953467392") return message.channel.send('non');
         const msg = await message.channel.send('Rebooting...');
+        msg.edit('Rebooted.(Wait 1-2 minutes.)');
         process.exit(1);
-        msg.edit('Rebooted.');
     }
 }
