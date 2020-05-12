84d5f50c8d2515860a471cf1f2f383f080881c44
SonMaxime <47661880+SonMaxime@users.noreply.github.com>
2020-05-13 00:34:49 +0200

    Update pp.js

--- commandes/Commandes d'info/pp.js
+++ commandes/Commandes d'info/pp.js
@@ -5,13 +5,10 @@ module.exports = {
     description: 'Affiche la pp de qui tu veux.',
     usage: '[@user]',
     run: async (client, message, args) => {
-        if (!message.mentions.users.size) {
-            return message.channel.send(`Ta pp: ${message.author.displayAvatarURL}`);
-        }
-
-        const avatarList = message.mentions.users.map(user => {
-            return `L'avatar de ${user.username} : ${user.displayAvatarURL}`;
-        });
-        message.channel.send(avatarList);
+        const embed = new MessageEmbed()
+        .setImage(message.author.displayAvatarURL)
+        .setColor('#275BF0')
+        
+        message.channel.send(embed)
     },
 };
